0042ab2c893e3ef8f173a58e8a1faa6a14269b63
Hanseul Lee <lhs010107@gmail.com>
2024-12-27 00:20:10 +0900

    feat: Refresh 엔티티 구현

---
+++ src/main/java/ceos/vote/global/repository/RefreshRepository.java
@@ -0,0 +1,13 @@
+package ceos.vote.global.repository;
+
+import ceos.vote.domain.member.entity.Refresh;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.transaction.annotation.Transactional;
+
+public interface RefreshRepository extends JpaRepository<Refresh, String> {
+
+    Boolean existsByRefresh(String refresh);
+
+    @Transactional
+    void deleteByRefresh(String refresh);
+}
