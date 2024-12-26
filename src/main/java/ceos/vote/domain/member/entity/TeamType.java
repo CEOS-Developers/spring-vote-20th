c5f568fcf65f8288c4ce3d131b03057f22c0b6e8
Hanseul Lee <lhs010107@gmail.com>
2024-12-26 23:49:30 +0900

    refactor: enum 타입에 description 추가

--- src/main/java/ceos/vote/domain/member/entity/TeamType.java
+++ src/main/java/ceos/vote/domain/member/entity/TeamType.java
@@ -1,9 +1,21 @@
 package ceos.vote.domain.member.entity;
 
+import lombok.Getter;
+import lombok.ToString;
+
+@Getter
+@ToString
 public enum TeamType {
-    PHOTOGROUND,
-    ANGELBRIDGE,
-    PEDALGENIE,
-    CAKEWAY,
-    COFFEEDEAL
+
+    PHOTOGROUND("포토그라운드"),
+    ANGELBRIDGE("엔젤브릿지"),
+    PEDALGENIE("페달지니"),
+    CAKEWAY("케이크웨이"),
+    COFFEEDEAL("커피딜");
+
+    private final String description;
+
+    TeamType(String description) {
+        this.description = description;
+    }
 }
