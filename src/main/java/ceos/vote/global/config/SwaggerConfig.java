f006d1a536d69cc942a3a02e6577bcbf16ee09a4
Hanseul Lee <lhs010107@gmail.com>
2024-12-27 00:10:49 +0900

    chore: swagger 환경 설정

---
+++ src/main/java/ceos/vote/global/config/SwaggerConfig.java
@@ -0,0 +1,38 @@
+package ceos.vote.global.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+    @Bean
+    public OpenAPI openAPI() {
+        String jwt = "JWT";
+
+        // Security Requirement와 Security Scheme 설정
+        SecurityRequirement securityRequirement = new SecurityRequirement().addList(jwt);
+        Components components = new Components().addSecuritySchemes(jwt, new SecurityScheme()
+                .name(jwt)
+                .type(SecurityScheme.Type.HTTP)
+                .scheme("bearer")
+                .bearerFormat("JWT")
+        );
+
+        // OpenAPI 객체 반환
+        return new OpenAPI()
+                .components(components)
+                .info(apiInfo())
+                .addSecurityItem(securityRequirement);
+    }
+
+    private Info apiInfo() {
+        return new Info()
+                .title("CEOS 투표 사이트 API") // Swagger 메인 타이틀
+                .description("Ceos 투표 사이트 프-백 과제 API"); // Swagger 설명
+    }
+}
