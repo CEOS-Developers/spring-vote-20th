12774e8695deb43ce3c24f2e0052c696560362b3
Hanseul Lee <lhs010107@gmail.com>
2024-12-27 02:17:43 +0900

    feat: MemberResponseDto 구현

---
+++ src/main/java/ceos/vote/domain/member/dto/response/MemberResponseDto.java
@@ -0,0 +1,30 @@
+package ceos.vote.domain.member.dto.response;
+
+import ceos.vote.domain.member.entity.Member;
+import lombok.Builder;
+
+@Builder
+public record MemberResponseDto (
+
+        String userId,
+        String name,
+        String email,
+        String team,
+        String part,
+        Boolean voteBack,
+        Boolean voteFront,
+        Boolean voteTeam
+) {
+    public static MemberResponseDto from(Member member) {
+        return MemberResponseDto.builder()
+                .userId(member.getUserId())
+                .name(member.getName())
+                .email(member.getEmail())
+                .team(member.getTeam().getDescription())
+                .part(member.getPart().getDescription())
+                .voteBack(member.getVoteBack())
+                .voteFront(member.getVoteFront())
+                .voteTeam(member.getVoteTeam())
+                .build();
+    }
+}
