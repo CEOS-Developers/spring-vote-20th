e72cf7429fbceeda593191a1d6a92e459c189cc5
choiseoji <choiseoji123@naver.com>
2024-12-29 23:33:07 +0900

    feat:  teamController에서 서비스 코드 호출

--- src/main/java/ceos/vote/domain/team/controller/TeamController.java
+++ src/main/java/ceos/vote/domain/team/controller/TeamController.java
@@ -1,8 +1,8 @@
 package ceos.vote.domain.team.controller;
 
 import ceos.vote.domain.team.dto.TeamRequestDto;
-import ceos.vote.domain.team.entity.Team;
-import ceos.vote.global.repository.TeamRepository;
+import ceos.vote.domain.team.service.TeamService;
+import ceos.vote.global.common.response.CommonResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -14,13 +14,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class TeamController {
 
-    private final TeamRepository teamRepository;
+    private final TeamService teamService;
 
     @PostMapping
-    public void createTeam(@RequestBody TeamRequestDto teamRequestDto) {
+    public CommonResponse<Void> createTeam(@RequestBody TeamRequestDto teamRequestDto) {
 
-        Team team = teamRequestDto.toEntity();
-        teamRepository.save(team);
-
+        teamService.createTeam(teamRequestDto);
+        return new CommonResponse<>("팀 생성 성공");
     }
 }
